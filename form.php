79a6321901d693f34e9670ea893f608fbe9d3a4e
NOVA\maria <maria_hav@live.se>
2017-11-09 15:51:41 +0100

    Fix for exercise 2

--- form.php
+++ form.php
@@ -1,36 +1,3 @@
-<?php
-
-$curl = curl_init();
-
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "http://curl.dev/form.php",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"animal\"\r\n\r\nÖrn\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"name\"\r\n\r\nHenrik\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--",
-  CURLOPT_HTTPHEADER => array(
-    "cache-control: no-cache",
-    "content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
-    "postman-token: cb920ab8-e883-ae79-220e-7765be679963"
-  ),
-));
-
-$response = curl_exec($curl);
-$err = curl_error($curl);
-
-curl_close($curl);
-
-if ($err) {
-  echo "cURL Error #:" . $err;
-} else {
-  echo $response;
-}
-
-?>
-
 <form method="post" action="form.php">
     <label>Namn</label><br>
     <input name="name" type="text"><br>  
@@ -42,10 +9,12 @@ if ($err) {
 <?php
     require "config.php";
 
-    $nam = $_POST["name"];
-    $ani = $_POST["animal"];
+    if ($_POST) {
+        $nam = $_POST["name"];
+        $ani = $_POST["animal"];
 
-    $sql = "INSERT INTO form (name, animal)
-    VALUES(:nameuo, :animaluo)";
-    $intoDb = $pdo->prepare($sql);
-    $intoDb->execute (array(':nameuo' => $nam, ':animaluo' => $ani)); 
+        $sql = "INSERT INTO form (name, animal)
+        VALUES(:nameuo, :animaluo)";
+        $intoDb = $pdo->prepare($sql);
+        $intoDb->execute (array(':nameuo' => $nam, ':animaluo' => $ani)); 
+    }
